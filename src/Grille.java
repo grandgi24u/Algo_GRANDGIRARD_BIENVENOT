ba5a0a38ecb6693281e79b55c5de565f1c8b3bdb
Clément GRANDGIRARD <grandgirard.clement24@gmail.com>
2020-10-14 22:27:14 +0200

    Ajout de fonctionnalité

--- src/Grille.java
+++ src/Grille.java
@@ -1,15 +1,31 @@
 
 public class Grille {
 
-	public int[][] tab;
+	public String[][] tab;
 	
-	public Grille(int t) {
-		if(t>0) {
-			this.tab = new int[t][t];
-		}
+	public Grille(String args) {
+		
 	}
 	
 	public void creerGrille() {
 		
+	}
+	
+	public void changerVal(int i1, int i2, int i3, int y1, int y2, int y3) {
+		this.tab[i1][y1] = ".";
+		this.tab[i2][y2] = ".";
+		this.tab[i3][y3] = "o";
+	}
+	
+	public int getHauteur() {
+		return this.tab.length;
+	}
+	
+	public int getLargeur() {
+		return this.tab[0].length;
+	}
+	
+	public String[][] getTab(){
+		return this.tab;
 	}
 }
--- src/SoloNoble.java
+++ src/SoloNoble.java
@@ -3,16 +3,58 @@ public class SoloNoble {
 	
 	private Grille g;
 	
-	public SoloNoble(String gtxt) {
-		this.g = new Grille();
+	public SoloNoble(String args) {
+		if(args != null) {
+			this.g = new Grille(args);
+		}else {
+			this.g = new Grille(null);
+		}
+		
 	}
 	
-	public boolean resoudreSoloNoble(int billes, char[][] tab) {
-		return false;
+	public boolean resoudreSoloNoble(int billes) {
+		Boolean fin = false;
+		if(billes == 0) {
+			fin = true;
+		}else {
+			for(int i = 0; i < this.g.getHauteur(); i++) {
+				for(int y = 0; y < this.g.getLargeur(); y++) {
+					if(g.getTab()[i][y-1] == "o" && g.getTab()[i][y-2] == ".") {
+						this.g.changerVal(i,i,i,y,y-1,y-2);
+						fin = this.resoudreSoloNoble(billes-1);
+					}
+					if(g.getTab()[i][y+1] == "o" && g.getTab()[i][y+2] == ".") {
+						this.g.changerVal(i,i,i,y,y+1,y+2);
+						fin = this.resoudreSoloNoble(billes-1);
+					}
+					if(g.getTab()[i-1][y] == "o" && g.getTab()[i-2][y] == ".") {
+						this.g.changerVal(i,i-1,i-1,y,y,y);
+						fin = this.resoudreSoloNoble(billes-1);
+					}
+					if(g.getTab()[i+1][y] == "o" && g.getTab()[i+2][y] == ".") {
+						this.g.changerVal(i,i+1,i+2,y,y,y);
+						fin = this.resoudreSoloNoble(billes-1);
+					}
+				}
+			}
+		}
+		return fin;
+	}
+	
+	public String toString() {
+		String s = "";
+		for(int i = 0; i < this.g.getHauteur(); i++) {
+			for(int y = 0; y < this.g.getLargeur(); y++) {
+				s += g.getTab()[i][y];
+			}
+			s+="\n";
+		}
+		
+		return s;
 	}
 	
 	public static void main(String[] args) {
-		SoloNoble s = new SoloNoble();
+		SoloNoble s = new SoloNoble(args[0]);
 	}
 	
 }
