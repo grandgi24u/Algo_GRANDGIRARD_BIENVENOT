3443857a3fbfb435159a8186e4d3af8986c26002
Clément GRANDGIRARD <grandgirard.clement24@gmail.com>
2020-10-15 17:08:31 +0200

    Ajout de la java doc

--- src/Grille.java
+++ src/Grille.java
@@ -3,11 +3,26 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * La classe Grille.
+ * 
+ * @author Clement GRANDGIRARD
+ */
 public class Grille {
 
-	public String[][] tab;
+	/** Tableau de String tab */
+	public String[][] tab; 
+	
+	/** Nombre de billes nbBilles*/
 	public int nbBilles;
 	
+	
+	/**
+	 * Constructeur Grille
+	 *
+	 * @param File f, ficher a lire
+	 * @throws IOException
+	 */
 	public Grille(File f) throws IOException {
 		FileReader fr = new FileReader(f);  
 		BufferedReader br = new BufferedReader(fr); 
@@ -42,30 +57,81 @@ public class Grille {
 		this.tab = tablier;
 	}
 	
+	
+	/**
+	 * Methode tester Deplacement
+	 *
+	 * @param int i, y
+	 * @return boolean resultat
+	 */
+	public boolean testerDeplacement(int i1, int i2, int i3, int y1, int y2, int y3) {
+		Boolean resultat = false;
+		if(this.tab[i1][y1].equals("o") && this.tab[i2][y2].equals("o") && this.tab[i3][y3].equals(".")) {
+			resultat = true;
+		}
+		return resultat;
+	}
+	
+	
+	/**
+	 * Methode changer valeur
+	 *
+	 * @param int i, y
+	 */
 	public void changerVal(int i1, int i2, int i3, int y1, int y2, int y3) {
 		this.tab[i1][y1] = ".";
 		this.tab[i2][y2] = ".";
 		this.tab[i3][y3] = "o";
 	}
 	
+	
+	/**
+	 * Methode retour Arriere
+	 *
+	 * @param int i, y
+	 */
 	public void retourArriere(int i1, int i2, int i3, int y1, int y2, int y3) {
 		this.tab[i1][y1] = "o";
 		this.tab[i2][y2] = "o";
 		this.tab[i3][y3] = ".";
 	}
 	
+	
+	/**
+	 * Methode get Hauteur
+	 *
+	 * @return int hauteur du tableau
+	 */
 	public int getHauteur() {
 		return this.tab.length;
 	}
 	
+	
+	/**
+	 * Methode get largeur
+	 *
+	 * @return int largeur du tableau
+	 */
 	public int getLargeur() {
 		return this.tab[0].length;
 	}
 	
+	
+	/**
+	 * Methode get Nombre de billes
+	 *
+	 * @return int nbBilles, nombres de billes
+	 */
 	public int getNbBilles() {
 		return this.nbBilles;
 	}
 	
+	
+	/**
+	 * Methode get Tablier
+	 *
+	 * @return String[][] tab, tablier actuelle
+	 */
 	public String[][] getTab(){
 		return this.tab;
 	}
--- src/SoloNoble.java
+++ src/SoloNoble.java
@@ -3,15 +3,36 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * La classe SoloNoble.
+ * 
+ * @author Clement GRANDGIRARD
+ */
 public class SoloNoble {
 
+	/** La grille de soloNoble */
 	private Grille g;
+	
+	/** Liste resultat des etapes de la solution */
 	private ArrayList<String> resultat = new ArrayList<String>();
 
+	
+	/**
+	 * Constructeur SoloNoble
+	 *
+	 * @param Grille gr, nouvelle grille
+	 */
 	public SoloNoble(Grille gr){
 		this.g = gr;
 	}
 
+	
+	/**
+	 * Methode resoudre SoloNoble
+	 *
+	 * @param int billes, le nombre de billes restantes
+	 * @return boolean fin 
+	 */
 	public boolean resoudreSoloNoble(int billes) {
 		Boolean fin = false;
 		
@@ -22,7 +43,7 @@ public class SoloNoble {
 				for(int y = 0; y < this.g.getLargeur(); y++) {
 					String res = this.toString();
 					try{
-						if(g.getTab()[i][y].equals("o") && g.getTab()[i][y-1].equals("o") && g.getTab()[i][y-2].equals(".") && !fin) {
+						if(g.testerDeplacement(i,i,i,y,y-1,y-2) && !fin) {
 							this.g.changerVal(i,i,i,y,y-1,y-2);
 							fin = this.resoudreSoloNoble(billes-1);
 							if(!fin) {
@@ -34,7 +55,7 @@ public class SoloNoble {
 						}
 					}catch(Exception e){}
 					try{
-						if(g.getTab()[i][y].equals("o") && g.getTab()[i][y+1].equals("o") && g.getTab()[i][y+2].equals(".") && !fin) {
+						if(g.testerDeplacement(i,i,i,y,y+1,y+2) && !fin) {
 							this.g.changerVal(i,i,i,y,y+1,y+2);
 							fin = this.resoudreSoloNoble(billes-1);
 							if(!fin) {
@@ -46,7 +67,7 @@ public class SoloNoble {
 						}
 					}catch(Exception e){}
 					try{
-						if(g.getTab()[i][y].equals("o") && g.getTab()[i-1][y].equals("o") && g.getTab()[i-2][y].equals(".") && !fin) {
+						if(g.testerDeplacement(i,i-1,i-2,y,y,y) && !fin) {
 							this.g.changerVal(i,i-1,i-2,y,y,y);
 							fin = this.resoudreSoloNoble(billes-1);
 							if(!fin) {
@@ -58,7 +79,7 @@ public class SoloNoble {
 						}
 					}catch(Exception e){}
 					try{
-						if(g.getTab()[i][y].equals("o") && g.getTab()[i+1][y].equals("o") && g.getTab()[i+2][y].equals(".") && !fin) {
+						if(g.testerDeplacement(i,i+1,i+2,y,y,y) && !fin) {
 							this.g.changerVal(i,i+1,i+2,y,y,y);
 							fin = this.resoudreSoloNoble(billes-1);
 							if(!fin) {
@@ -75,13 +96,23 @@ public class SoloNoble {
 		}
 		return fin;
 	}
+	
 
+	/**
+	 * Methode afficher Resultat
+	 */
 	public void afficherResultat() {
 		for(int i = this.resultat.size()-1; i >= 0; i--) {
 			System.out.println(this.resultat.get(i));
 		}
 	}
 	
+	
+	/**
+	 * Methode ToString
+	 * 
+	 * @return String s, le tablier actuel
+	 */
 	public String toString() {
 		String s = "";
 		for(int i = 0; i < this.g.getHauteur(); i++) {
@@ -93,6 +124,12 @@ public class SoloNoble {
 		return s;
 	}
 
+	
+	/**
+	 * La methode main
+	 *
+	 * @param args les arguments
+	 */
 	public static void main(String[] args) throws IOException {
 		File f;
 		
